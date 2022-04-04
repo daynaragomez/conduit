d6cb7f6b691260069da512e961eaed31ed66cbb6
daynara <99415785+daynaragomez@users.noreply.github.com>
2022-04-04 14:23:39 -0400

    test with Backend and Headless authorization

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,3 @@
-/// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -12,11 +11,21 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile (file) {
+  const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
+  if(!fs.existsSync(pathToConfigFile)){
+    return {};
+  }
+
+  return fs.readJson(pathToConfigFile)
+}
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+
+  const file = config.env.configFile
+
+  return getConfigurationByFile(file)
 }
